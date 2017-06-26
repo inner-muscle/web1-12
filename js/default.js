1e7141296ff01e67c0d797282abd27ad8c6578b5
inner-muscle <tsuruua1223@gmail.com>
2017-06-26 17:12:35 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,16 +1,7 @@
-for(var i = 1;i<10;i++)
+for(var i = 1;i<100;i++)
 {
   var li = document.createElement('li');
-  li.textContent = i + '回目';
+  li.textContent = i ;
   document.getElementById('list').appendChild(li);
 }
 
-var HP=100;
-while(0<HP)
-{
-  var li = document.createElement('li');
-  var damage = Math.floor(Math.random()*100);
-  HP -= damage;
-  HP = (HP < 0) ? 0: HP;
-  li.textContent = damage + 'のダメージを受けた。残りのHP' + HP;;
-  document.getElementById('while').appendChild(li);
