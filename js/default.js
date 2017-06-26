da3c2cb8391599496f8afab367c8b8aa9690cd2e
inner-muscle <tsuruua1223@gmail.com>
2017-06-26 17:32:45 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,11 +1,7 @@
-for(var i = 1;i<100;i++)
-{
-  var li = document.createElement('li');
-  li.textContent = i ;
-  document.getElementById('list').appendChild(li);
-}
-
 document.getElementById('form').onsubmit = function()
 {
   window.alert(document.getElementById('form').word.value);
 };
+
+
+  document.getElementById('list').appendChild(word);
